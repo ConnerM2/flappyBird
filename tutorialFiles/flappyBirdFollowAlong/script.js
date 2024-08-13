d7d2bbbf6771189e5fc57ad4c55f18f555926f4b
Conner Manwill <140928079+ConnerM2@users.noreply.github.com>
2024-08-12 18:04:27 -0600

    added JS that inserts the bird PNG to the canvas. Also JS that creates object imgs for the pipes

--- tutorialFiles/flappyBirdFollowAlong/script.js
+++ tutorialFiles/flappyBirdFollowAlong/script.js
@@ -5,12 +5,14 @@ let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
-// bird
+// bird, we are defining the demesions and positions in JS
 let birdWidth = 34;
 let birdHeight = 24;
 let birdX = boardWidth/8;
 let birdY = boardHeight/2;
+let birdImg;
 
+// object of bird to easlily acces the data above
 let bird = {
     x : birdX,
     y : birdY,
@@ -24,7 +26,17 @@ window.onload = function () {
     board.width = boardWidth
     context = board.getContext('2d'); //this is used for drawing on the canvas
 
-    // draw flappy bird
-    context.fillStyle = "green"
-    context.fillRect(bird.x, bird.y, bird.width, bird.height)
+    // this draws the bird PNG onto the canvas
+    // it creates an object that lets us manipulate it
+    birdImg = new Image();
+    birdImg.src = "../flappy-bird/flappybird.png"
+    birdImg.onload = function () {
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height)
+    }
+
+    topPipeImg = new Image();
+    topPipeImg.src = "../flappy-bird/toppipe.png"
+
+    bottomPipeImg = new Image();
+    bottomPipeImg.src = "../flappy-bird/bottompipe.png"
 }
