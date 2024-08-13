4594c2bf311cdd077840898102c486d7db08bfbd
Conner Manwill <140928079+ConnerM2@users.noreply.github.com>
2024-08-13 10:25:56 -0600

    Added code to randomly put top pipes, and added the bottom pipes

--- tutorialFiles/flappyBirdFollowAlong/script.js
+++ tutorialFiles/flappyBirdFollowAlong/script.js
@@ -86,14 +86,30 @@ function update () {
 }
 
 function placePipes () {
+
+    // this sets the pipe at a random position
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random()*(pipeHeight/2);
+    let openingSpace = board.height/4
+
     let topPipe = {
         img : topPipeImg,
         x : pipeX,
-        y : pipeY,
+        y : randomPipeY,
         width : pipeWidth,
         height : pipeHeight,
         passed : false
     }
 
     pipeArray.push(topPipe)
+
+    let bottomPipe = {
+        img : bottomPipeImg,
+        x : pipeX,
+        y : randomPipeY + pipeHeight + openingSpace,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed : false
+    }
+
+    pipeArray.push(bottomPipe)
 }
